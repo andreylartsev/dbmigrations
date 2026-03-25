247bd1b1dd890be74d1390a15bd3de3f629b7fd3
Andrey Lartsev <andrey.lartsev@gmail.com>
2026-03-25 10:36:36 +0300

    added requirements.txt

--- dbmigrations/dbmigration.py
+++ dbmigrations/dbmigration.py
@@ -21,42 +21,42 @@ import tomllib
 import psycopg;
 
 def __test_connect_to_db():
-    conn = psycopg.connect("dbname=postgres")
+    conn = psycopg.connect("dbname=postgres host=localhost user=postgres password=1234561")
 
 class UpdateCommand:
     """Applies baseline, versioned and repeatable scripts within target database schema"""
     def __init__(self, subparsers):
         parser = subparsers.add_parser("update", help=UpdateCommand.__doc__)
-        parser.add_argument("schema_name", type=str, help="target database schema")
-        parser.add_argument("repo_path", type=str, help="source scripts repository path")
+        parser.add_argument("schema_name", type=str, help="the name of target database schema")
+        parser.add_argument("scripts_path", type=str, help="source scripts repository path")
         parser.add_argument("-s","--skip-signature-check", action="store_true", default=False, help="to skip the signature check")
         parser.set_defaults(exec=self) 
     def __call__(self, args):
-        print(f"Apply updates repo_path={args.repo_path}, schema_name={args.schema_name}, skip_signature_check={args.skip_signature_check}")
+        print(f"Apply updates scripts_path={args.scripts_path}, schema_name={args.schema_name}, skip_signature_check={args.skip_signature_check}")
 
 class VerifyCommand:
     """Verifies target schema and lists versioned and repatable scripts to be applied within"""
     def __init__(self, subparsers):
         parser = subparsers.add_parser("verify", help=VerifyCommand.__doc__)
-        parser.add_argument("schema_name", type=str, help="target database schema")
-        parser.add_argument("repo_path", type=str, help="source scripts repository path")
+        parser.add_argument("schema_name", type=str, help="the name of target database schema")
+        parser.add_argument("scripts_path", type=str, help="source scripts repository path")
         parser.add_argument("-b","--build-update-script", action="store_true", default=False, help="to build and list the whole SQL script instead of just listing script files")
         parser.add_argument("-s","--skip-signature-check", action="store_true", default=False, help="to skip the signature check")
         parser.set_defaults(exec=self) 
     def __call__(self, args):
-        print(f"Verify database schema repo_path={args.repo_path}, schema={args.schema_name}, {args.build_update_script}, skip_signature_check={args.skip_signature_check}")
+        print(f"Verify database schema scripts_path={args.scripts_path}, schema={args.schema_name}, {args.build_update_script}, skip_signature_check={args.skip_signature_check}")
 
 class InitCommand:
     """Creates version control tables in the empty database schema"""
     def __init__(self, subparsers):
         parser = subparsers.add_parser("init", help=InitCommand.__doc__)
-        parser.add_argument("schema_name", type=str, help="target database schema")
-        parser.add_argument("repo_path", type=str, help="source scripts repository path")
+        parser.add_argument("schema_name", type=str, help="the name of target database schema")
+        parser.add_argument("scripts_path", type=str, help="source scripts repository path")
         parser.add_argument("-f","--force-init", action="store_true", default=False, help="to create version control tables even if schema is NOT empty")
         parser.add_argument("-s","--skip-signature-check", action="store_true", default=False, help="to skip the signature check")
         parser.set_defaults(exec=self) 
     def __call__(self, args):
-        print(f"Creates version control tables repo_path={args.repo_path}, schema={args.schema_name}, force_init={args.force_init}, skip_signature_check={args.skip_signature_check}")
+        print(f"Creates version control tables scripts_path={args.scripts_path}, schema={args.schema_name}, force_init={args.force_init}, skip_signature_check={args.skip_signature_check}")
 
 
 def main():
@@ -81,5 +81,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-    #__test_connect_to_db()
+    __test_connect_to_db()
 
